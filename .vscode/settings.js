a53788e4ee07ca211f5f444230d736d2c5e9a24a
wenkang <WENKANG.YANG@DELTAWW.COM>
2020-10-23 13:49:05 +0800

    feat: add eslint

---
+++ .vscode/settings.js
@@ -0,0 +1,33 @@
+// Place your settings in this file to overwrite default and user settings.
+{
+  "deepscan.enable": true,
+  "files.eol": "\n",
+  // Support using flow through your node_modules folder, WARNING: Checking this box is a security risk. When you open a project we will immediately run code contained within it.
+  "flow.useNPMPackagedFlow": true,
+  // Enable/disable JavaScript validation. (For Flow)
+  "javascript.validate.enable": false,
+  // Enable/disable default JavaScript formatter (For Prettier)
+  "javascript.format.enable": false,
+  "editor.tabSize": 2,
+  "editor.insertSpaces": true,
+  "window.zoomLevel": -1,
+  "prettier.eslintIntegration": true,
+  "files.autoSave": "off",
+  "prettier.singleQuote": true,
+  "editor.formatOnSave": true,
+  "gitlens.advanced.messages": {
+    "suppressCommitHasNoPreviousCommitWarning": false,
+    "suppressCommitNotFoundWarning": false,
+    "suppressFileNotUnderSourceControlWarning": false,
+    "suppressGitVersionWarning": false,
+    "suppressLineUncommittedWarning": false,
+    "suppressNoRepositoryWarning": false,
+    "suppressResultsExplorerNotice": false,
+    "suppressShowKeyBindingsNotice": true,
+    "suppressUpdateNotice": false,
+    "suppressWelcomeNotice": true
+  },
+  "gitlens.keymap": "alternate",
+  "spellright.language": ["en"],
+  "spellright.documentTypes": ["markdown", "latex", "plaintext"]
+}
--- pages/home/store/reducer.js
+++ pages/home/store/reducer.js
@@ -1,37 +1,37 @@
-import {TICK,INCREMENT,DECREMENT,RESET} from './constants.js'
+import { TICK, INCREMENT, DECREMENT, RESET } from "./constants.js";
 
 const initialState = {
   lastUpdate: 0,
   light: false,
   count: 0,
-} 
+};
 const countReducer = (state = initialState, action) => {
-  console.log(9,action)
+  console.log(9, action);
   switch (action.type) {
     case TICK:
       return {
         ...state,
         lastUpdate: action.lastUpdate,
         light: !!action.light,
-      }
+      };
     case INCREMENT:
       return {
         ...state,
         count: state.count + 1,
-      }
+      };
     case DECREMENT:
       return {
         ...state,
         count: state.count - 1,
-      }
+      };
     case RESET:
       return {
         ...state,
         count: initialState.count,
-      }
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default countReducer
+export default countReducer;
